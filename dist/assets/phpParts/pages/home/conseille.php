feedad476fd8bbc6ea52f6e8ea016c85825fbf63
vincent.maret <vincent.maret@verallia.com>
2019-08-29 17:31:35 +0200

    start conseille page

--- dist/assets/phpParts/pages/home/conseille.php
+++ dist/assets/phpParts/pages/home/conseille.php
@@ -1,3 +1,40 @@
 <div class="section-contents">
+  <h3>
+    VOUS SOUHAITEZ EN SAVOIR PLUS SUR LE CPF DE TRANSITION 
+    PROFESSIONNELLE OU PRÉPARER VOTRE PROJET DE FORMATION ?
+    <br>
+    LAISSEZ-NOUS VOUS CONTACTER.
+  </h3>
 
+  <form action="" method="post">
+    <ul>
+      <li>
+
+        <div>1.&nbsp;</div>
+        <div>
+            <P>
+            Avez-vous déjà contacté le Fongecif Île-de-France et/ou avez-vous déjà créé votre espace personnel sur www.fongecif-idf.fr ?
+          </P>
+
+          <div class="cb-container">
+            <label>   
+               <input type="radio" name="q1" value="Oui">
+               <span class="fake-cb"></span>
+               <span class="label-txt">Oui</span>
+            </label>
+          </div>
+
+          <div class="cb-container">
+            <label>
+              <input type="radio" name="q1" value="Non">
+              <span class="fake-cb"></span>
+              <span class="label-txt">Non</span>
+            </label>
+          </div>
+        </div>
+        
+      </li>
+
+    </ul>
+  </form>
 </div>
--- dist/assets/phpParts/pages/home/reconversion.php
+++ dist/assets/phpParts/pages/home/reconversion.php
@@ -10,62 +10,88 @@
     </p>
   </div>
 
-  <div class="section-part section-part-2 d-flex flex-column flex-sm-row">
+  <div class="section-part two-parts-row section-part-2 d-flex flex-column flex-sm-row">
 
     <div>
-      <img class="triangle" src="assets/img/home/reconversion/triangle-w.png" alt="triangle">
-      <h4>1<br>NOUS VOUS CONTACTONS</h4>
-      <img src="assets/img/home/reconversion/phone.png" alt="">
+      <div>
+        <h4>1<br>NOUS VOUS CONTACTONS</h4>
+        <img class="pic" src="assets/img/home/reconversion/phone.png" alt="">
+      </div>
     </div>
 
     <div>
-      Nous prenons contact avec vous par téléphone pour faire le point sur votre situation professionnelle ou de votre
-      projet de reconversion
-    </div>
-
-  </div>
-
-  <div class="section-part section-part-3 d-flex flex-column flex-sm-row">
-
-    <div>
-      <img class="triangle" src="assets/img/home/reconversion/triangle-o.png" alt="triangle">
-      <h4>2<br>NOUS VOUS CONTACTONS</h4>
-      <img src="assets/img/home/reconversion/question.png" alt="">
-    </div>
-
-    <div>
-      Nous prenons contact avec vous par téléphone pour faire le point sur votre situation professionnelle ou de votre
-      projet de reconversion
+      <p>
+        Nous prenons contact avec vous par téléphone pour faire le point sur votre situation professionnelle ou de votre
+        projet de reconversion.
+      </p>
     </div>
 
   </div>
 
-  <div class="section-part section-part-4 d-flex flex-column flex-sm-row">
+  <div class="section-part two-parts-row section-part-3 d-flex flex-column flex-sm-row">
 
-    <div>
-      <img class="triangle" src="assets/img/home/reconversion/triangle-w.png" alt="triangle">
-      <h4>3<br>NOUS VOUS CONTACTONS</h4>
-      <img src="assets/img/home/reconversion/feuille.png" alt="">
+     <div>
+      <div>
+        <img class="triangle" src="assets/img/home/reconversion/triangle-o.png" alt="triangle">
+        <h4>2<br>JE RENCONTRE MON CONSEILLER</h4>
+        <img class="pic" src="assets/img/home/reconversion/question.png" alt="">
+      </div>
     </div>
 
     <div>
-      Nous prenons contact avec vous par téléphone pour faire le point sur votre situation professionnelle ou de votre
-      projet de reconversion
+      <p>
+        Un de nos conseillers vous accueille pour vous 
+        accompagner dans votre réflexion et vos démarches 
+        administratives et financières. Il vous aide dans la 
+        construction de votre projet de reconversion dans le 
+        cadre d'un suivi personnalisé jusqu'à sa mise en œuvre.
+      </p>
     </div>
 
   </div>
 
-  <div class="section-part section-part-5 d-flex flex-column flex-sm-row">
+  <div class="section-part two-parts-row section-part-4 d-flex flex-column flex-sm-row">
 
-    <div>
-      <img class="triangle" src="assets/img/home/reconversion/triangle-o.png" alt="triangle">
-      <h4>4<br>NOUS VOUS CONTACTONS</h4>
-      <img src="assets/img/home/reconversion/main.png" alt="">
+     <div>
+      <div>
+        <img class="triangle" src="assets/img/home/reconversion/triangle-w.png" alt="triangle">
+        <h4>3<br>NOUS ÉLABORONS UN PLAN D’ACTIONS</h4>
+        <img class="pic" src="assets/img/home/reconversion/feuille.png" alt="">
+      </div>
     </div>
 
     <div>
-      Nous prenons contact avec vous par téléphone pour faire le point sur votre situation professionnelle ou de votre
-      projet de reconversion
+      <p>
+        Votre projet est cohérent et solide ?
+        Nous élaborons à vos côtés un plan d’actions et vous 
+        aidons à le mettre en œuvre : identification de 
+        l’organisme de formation et des modes de financement…
+      </p>
+    </div>
+
+  </div>
+
+  <div class="section-part two-parts-row section-part-5 d-flex flex-column flex-sm-row">
+
+     <div>
+      <div>
+        <img class="triangle" src="assets/img/home/reconversion/triangle-o.png" alt="triangle">
+        <h4>4<br>VOTRE PROJET EST FORMALIS&Eacute;</h4>
+        <img class="pic" src="assets/img/home/reconversion/main.png" alt="">
+      </div>
+    </div>
+
+    <div>
+      <div>
+        <p>
+        Un document récapitulant la description de votre projet 
+        professionnel vous est remis à chaque étape. Vous êtes prêt à vous lancer !
+      </p>
+
+       <a href="https://youtu.be/P701paKEMXs?list=LLMd8Lo2Mkhjjb6URrhDGSUA" target="_blank">
+        EN SAVOIR PLUS
+      </a>
+      </div>
     </div>
 
   </div>
