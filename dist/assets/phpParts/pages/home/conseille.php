6329c3d47e7c7b1fd8929ca27450f816aa597086
Vincent Maret <vincent.maret@verallia.com>
2019-08-29 21:50:44 +0200

    finish conseille page

--- dist/assets/phpParts/pages/home/conseille.php
+++ dist/assets/phpParts/pages/home/conseille.php
@@ -1,6 +1,6 @@
 <div class="section-contents">
   <h3>
-    VOUS SOUHAITEZ EN SAVOIR PLUS SUR LE CPF DE TRANSITION 
+    VOUS SOUHAITEZ EN SAVOIR PLUS SUR LE CPF DE TRANSITION
     PROFESSIONNELLE OU PRÉPARER VOTRE PROJET DE FORMATION ?
     <br>
     LAISSEZ-NOUS VOUS CONTACTER.
@@ -9,18 +9,18 @@
   <form action="" method="post">
     <ul>
       <li>
-
         <div>1.&nbsp;</div>
         <div>
-            <P>
-            Avez-vous déjà contacté le Fongecif Île-de-France et/ou avez-vous déjà créé votre espace personnel sur www.fongecif-idf.fr ?
+          <P class="form-part-title">
+            Avez-vous déjà contacté le Fongecif Île-de-France et/ou avez-vous déjà créé votre espace personnel sur
+            <a class="link-txt" href="http://www.fongecif-idf.fr" target="_blank">www.fongecif-idf.fr</a>&nbsp;?
           </P>
 
           <div class="cb-container">
-            <label>   
-               <input type="radio" name="q1" value="Oui">
-               <span class="fake-cb"></span>
-               <span class="label-txt">Oui</span>
+            <label>
+              <input type="radio" name="q1" value="Oui">
+              <span class="fake-cb"></span>
+              <span class="label-txt">Oui</span>
             </label>
           </div>
 
@@ -32,9 +32,162 @@
             </label>
           </div>
         </div>
-        
       </li>
 
+      <li>
+        <div>2.&nbsp;</div>
+        <div>
+          <P class="form-part-title">
+            Vous êtes en :
+          </P>
+
+          <div class="cb-container">
+            <label>
+              <input type="radio" name="q2" value="CDI">
+              <span class="fake-cb"></span>
+              <span class="label-txt">CDI</span>
+            </label>
+          </div>
+
+          <div class="cb-container">
+            <label>
+              <input type="radio" name="q2" value="CDD">
+              <span class="fake-cb"></span>
+              <span class="label-txt">CDD</span>
+            </label>
+          </div>
+
+          <div class="cb-container">
+            <label>
+              <input type="radio" name="q2" value="Autre">
+              <span class="fake-cb"></span>
+              <span class="label-txt">Autre</span>
+            </label>
+          </div>
+        </div>
+      </li>
+
+      <li>
+        <div>3.&nbsp;</div>
+        <div>
+          <P class="form-part-title">
+            Vous avez un projet de reconversion professionnelle :
+          </P>
+
+          <div class="cb-container">
+            <label>
+              <input type="radio" name="q3" value="A court terme">
+              <span class="fake-cb"></span>
+              <span class="label-txt">&Agrave; court terme</span>
+            </label>
+          </div>
+
+          <div class="cb-container">
+            <label>
+              <input type="radio" name="q3" value="A moyen terme">
+              <span class="fake-cb"></span>
+              <span class="label-txt">&Agrave; moyen terme</span>
+            </label>
+          </div>
+
+          <div class="cb-container">
+            <label>
+              <input type="radio" name="q3" value="A long terme">
+              <span class="fake-cb"></span>
+              <span class="label-txt">&Agrave; long terme</span>
+            </label>
+          </div>
+        </div>
+      </li>
+
+      <li>
+        <div>4.&nbsp;</div>
+        <div>
+          <P class="form-part-title">
+            &Agrave; quel moment de la journée souhaitez-vous être appelé ?
+          </P>
+
+          <div class="cb-container">
+            <label>
+              <input type="checkbox" name="q4" value="Entre 9h et 12h">
+              <span class="fake-cb"></span>
+              <span class="label-txt">Entre 9h et 12h</span>
+            </label>
+          </div>
+
+          <div class="cb-container">
+            <label>
+              <input type="checkbox" name="q4" value="Entre 12h et 14h">
+              <span class="fake-cb"></span>
+              <span class="label-txt">Entre 12h et 14h</span>
+            </label>
+          </div>
+
+          <div class="cb-container">
+            <label>
+              <input type="checkbox" name="q4" value="Entre 14h et 16h">
+              <span class="fake-cb"></span>
+              <span class="label-txt">Entre 14h et 16h</span>
+            </label>
+          </div>
+
+          <div class="cb-container">
+            <label>
+              <input type="checkbox" name="q4" value="Entre 16h et 18h">
+              <span class="fake-cb"></span>
+              <span class="label-txt">Entre 16h et 18h</span>
+            </label>
+          </div>
+        </div>
+      </li>
+
+      <li class="form-info">
+        <div>5.&nbsp;</div>
+        <div>
+          <P class="form-part-title">
+            Vos informations
+          </P>
+
+          <label>
+            <span>Nom</span>
+            <input type="text" name="LastName">
+          </label>
+
+          <label>
+            <span>Prénom</span>
+            <input type="text" name="FirstName">
+          </label>
+
+          <label>
+            <span>Numéro de téléphone</span>
+            <input type="text" name="Phone">
+          </label>
+
+          <label>
+            <span>Adresse email</span>
+            <input type="text" name="Mail">
+          </label>
+        </div>
+      </li>
+
+      <li class="acceptation">
+        <P class="form-part-title">
+          Que faites-vous de mes données&nbsp;?
+        </P>
+
+        <div class="cb-container">
+          <label>
+            <input type="checkbox" name="acceptation">
+            <span class="fake-cb"></span>
+            <span class="label-txt">
+              En soumettant ce formulaire, j’accepte que les informations saisies soient
+              exploitées dans le cadre de la demande.
+            </span>
+          </label>
+        </div>
+
+        <input type="submit" value="ENVOYER">
+      </li>
     </ul>
   </form>
 </div>
